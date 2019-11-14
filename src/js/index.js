b5654954d1f8678c9db26c98d113f3fdfb705afd
Ivan Bazaldua <icesarbr@gmail.com>
2019-11-13 23:13:07 -0600

    Agregado Observer a la navegación principal para agregar o quitar clases que modifican su apariencia

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,54 @@
 
+const path = require('path');
 /* Estilos*/
 require('normalize.css');
 import "../styles/main.scss";
+import "../styles/index.scss";
+
 /* Imagenes*/
+/*
+***** REVISAR LAS RUTAS A IMAGENES DESDE ARCHIVOS CSS 
+ CUANDO SE LANZE A PRODUCIION 
+*/
 import "../static/iconoTeotl.png";
+import "../static/panorama-Monterrey-NL-OPT.jpg";
 
 
 
 /* Codigo JS*/
-eventListeners();
+const heroHome = document.querySelector('.hero-home');
+const header = document.querySelector('.header');
+const heroImage = document.querySelector('.header-logo');
+const mainNavLinks = document.querySelectorAll('.nav-main__desktop a');
+let mainNavLinksArr = Array.from(mainNavLinks);
+console.log(mainNavLinks);
+
+const heroOptions = {
+    rootMargin: "-140px"
+}
+
+const heroObserver = new IntersectionObserver( function( entries,heroObserver ){
+    entries.forEach( entry => {
+        if(!entry.isIntersecting){
+            header.classList.add("header-scrolled");
+            heroImage.classList.add("headerImg-scrolled");
+            //mainNavLinks.classList.add("nav-main__desktop__item-scrolled");
+            mainNavLinksArr.forEach( link =>{
+                link.classList.add("nav-main__desktop__item-scrolled");
+            });
+        } else {
+            header.classList.remove("header-scrolled");
+            heroImage.classList.remove("headerImg-scrolled");
+            //mainNavLinks.classList.remove("nav-main__desktop__item-scrolled");
+            mainNavLinksArr.forEach( link =>{
+                link.classList.remove("nav-main__desktop__item-scrolled");
+            });
+        }
+    });
+}, heroOptions );
+
+heroObserver.observe(heroHome);
+//eventListeners();
 
 function eventListeners(){
     window.addEventListener('scroll', () =>{
